665b249e074f63f723983d125ac6b445e24a9343
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-03-08 02:00:12 +0530

    yup

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -6,15 +6,18 @@ import {SliderProducts} from '../../data/products'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
+import './Slider.css'
 
 
 const Slider = () => {
   return (
     <div className="s-container">
-        <Swiper sidesPerView={3} spaceBetween={40} slidesPerGroup={1} loop={true} >
+        <Swiper modules={[Pagination,Navigation]} className='mySwiper' loopFillGroupWithBlank={true}
+        navigation={true}
+        slidesPerView={3} spaceBetween={40} slidesPerGroup={1} loop={true} >
           {SliderProducts.map((slide,i)=>(
             <SwiperSlide>
-              <div className='slide'>
+              <div className='details'>
                 <div className='name'>
                   <span>{slide.name}</span>
                   <span>{slide.detail}</span>
